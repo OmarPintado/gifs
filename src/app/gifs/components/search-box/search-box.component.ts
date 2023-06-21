449caf89baff50a351213fe2547325cb66bc4eeb
GaOmPS <omarpintado98@gmail.com>
2023-06-21 12:42:37 -0500

    paso 86

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-box.component.css']
 })
 export class SearchBoxComponent {
+  @ViewChild('searchBox')
+  public tagInputXD!:ElementRef<HTMLInputElement>;
 
+  searchTag():void {
+    const newTag = this.tagInputXD.nativeElement.value;
+    console.log({xdTag: newTag});
+  }
 }
