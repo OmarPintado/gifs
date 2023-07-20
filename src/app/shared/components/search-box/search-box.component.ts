201e9abf0b326d84d905b8b67bb13fc6d0bdc83b
GaOmPS <omarpintado98@gmail.com>
2023-07-19 23:39:27 -0500

    134 | Country SPA [Loading]

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {debounceTime, Subject} from "rxjs";
 
 @Component({
   selector: 'shared-search-box',
@@ -6,14 +7,29 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
   @Input()
   public placeholder:string = '';
 
   @Output()
   public onValue:EventEmitter<string> = new EventEmitter();
+  private tuboDeAgua:Subject<string> = new Subject<string>();
+  ngOnInit():void {
+    this.tuboDeAgua.pipe(
+      debounceTime(250)
+    )
+      .subscribe(value => {
+        this.onValue.emit(value)
+      })
+  }
+
   enterBuscar(algoBusca:string):void{
     this.onValue.emit(algoBusca);
+  }
+
+
+  onKeyPress(searchTerm:string):void{
+    this.tuboDeAgua.next(searchTerm)
   }
 }
 
